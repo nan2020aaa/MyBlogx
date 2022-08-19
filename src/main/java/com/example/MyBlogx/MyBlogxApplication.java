43eb8b553de3e1826e9527b5f7c36e033cfee68c
yangzhennan <1010976078@gmail.com>
2022-08-19 10:48:01 +0900

    springbootのフレームワークをrepositoryの中に入れました。

---
+++ src/main/java/com/example/MyBlogx/MyBlogxApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyBlogx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyBlogx/MyBlogxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyBlogx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
